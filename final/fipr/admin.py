b8c6068a5f8cf4928d408b3641ad70504a4fdb06
programmerabdullokh <abdullokhnabiev998@gmail.com>
2024-10-19 15:58:26 +0900

    qwerty

---
+++ final/fipr/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+admin.site.register(Information)
+admin.site.register(Service)
+admin.site.register(Order)
+admin.site.register(Doctor)
+admin.site.register(Contact)
